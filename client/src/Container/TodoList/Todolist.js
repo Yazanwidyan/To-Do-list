72b0322db31588cb1f42359b49867b70eb9a6af9
Yazanwidyan <yazanwidyan1@gmail.com>
2020-01-09 18:37:52 +0200

    add delete function

--- client/src/Container/TodoList/Todolist.js
+++ client/src/Container/TodoList/Todolist.js
@@ -3,38 +3,86 @@ import "./TodoList.css";
 
 class TodoList extends React.Component {
   state = {
-    arr : [],
-    items : ''        
-
+    arr: [],
+    items: "",
+    isInEditMode: false
   };
-  
-  handleChange =(e)=>{
-    this.setState({items : e.target.value})
-  }
-  handleSubmit=()=>{
-    const qays= this.state.arr;
-    qays.push(this.state.items)
-    
-    this.setState({arr: qays})
-  }
-  handleDelete =()=>{
-    
+
+  handleChange = e => {
+    this.setState({ items: e.target.value });
+  };
+  handleSubmit = () => {
+    const join = this.state.arr;
+    join.push(this.state.items);
+
+    this.setState({ arr: join });
+  };
+
+  /*handleDelete= (index) =>{
+    const postArray = Object.assign([], this.state.arr);
+    postArray.splice(index,1);
+    this.setState({ arr : postArray})
   }
 
+ /* handleDelete = (index) => {
+    const myItems = this.state.arr.filter(item=> {
+      return item.index !== index
+    })
+    this.setState({items : myItems})
+  };*/
+
+  handleDelete = index => {
+    const myItems = this.state.arr;
+    const x = myItems.findIndex(item => item.index === index);
+    myItems.splice(x);
+    this.setState({ arr: myItems });
+  };
+
+  changeEdit = () => {
+    this.setState({
+      isInEditMode: !this.state.isInEditMode
+    });
+  };
+
+  /*updateText=()=>{
+ const newTxt= this.state.arr.map((items) => (
+     {items}))
+     this.setState({ newTxt:newTxt})
+
+  
+ }
+*/
   render() {
     return (
       <div className="mainTodo">
         <input
+          placeholder=" Type here"
           type="text"
-          value ={this.state.items}
-          onChange = {this.handleChange}
+          value={this.state.items}
+          onChange={this.handleChange}
         />
         <button className="btn3" type="submit" onClick={this.handleSubmit}>
           Add
         </button>
-        <div>
-          {this.state.arr.map((items, index)=> <div key={index}>{items} <button >Delete</button></div>)}
-        </div>
+        {this.state.isInEditMode ? (
+          <div>
+            <button> Save</button>
+            <input type="text" defaultValue={this.state.items} />
+          </div>
+        ) : (
+          <div>
+           
+            <span>Todo List</span>
+            {this.state.arr.map((item, index) => (
+              <div key={index}>
+                {item} 
+                <input className="check" type="checkbox" value="on" />
+                <button onClick={this.handleDelete}>Delete</button>
+                <button onClick={this.changeEdit}>Edit</button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     );
   }
